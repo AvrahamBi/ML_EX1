df544ef04d07ae365ccc3e7ff87a37ece48bad54
AvrahamBi <avrahambarilan@gmail.com>
2021-11-07 17:01:21 +0200

    little progress

--- ex1.py
+++ ex1.py
@@ -5,7 +5,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-
+def distance3D(a, b):
+    a = np.array([float(a[0]), float(a[1]), float(a[2])])
+    b = np.array([float(b[0]), float(b[1]), float(b[2])])
+    return np.linalg.norm(a - b)
 
 def main():
     # python ex1.py <image path> <centroids init path> <output>
@@ -15,7 +18,22 @@ def main():
     pixels = orig_pixels.astype(float) / 255.
     # Reshape the image(128x128x3) into an Nx3 matrix where N = number of pixels.
     pixels = pixels.reshape(-1, 3)
-    print()
+
+    # open centroids file
+    with open(centroids_fname) as file:
+        centroids = file.readlines()
+        centroids = [line.rstrip().split(" ") for line in centroids]
+    file.close()
+
+    a = np.array([float(centroids[0][0]), float(centroids[0][1]), float(centroids[0][2])])
+    b = np.array([float(centroids[1][0]), float(centroids[1][1]), float(centroids[1][2])])
+   dist = distance3D(centroids[0], centroids[1])
+
+    isChanged = True
+    #while(isChanged):
+      #  for pixel in pixels:
+
+
 
 
 
